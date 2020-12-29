006b9cb782303cceee15bf991d82adaa40f1455a
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2020-12-29 12:24:46 -0500

    birthday cake exercise

--- minimaxsum.py
+++ minimaxsum.py
@@ -1,21 +1,40 @@
-#!/bin/python3
+# #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
+# import math
+# import os
+# import random
+# import re
+# import sys
+# from itertools import combinations
+
+# # Complete the miniMaxSum function below.
+# def miniMaxSum(arr):
+#     combos = list(combinations(arr, 4)) # https://www.geeksforgeeks.org/python-itertools/
+#     combo_sums = []
+#     for combo in combos:
+#         combo_sums.append(sum(combo))
+#     print(min(combo_sums), max(combo_sums))
+
+# if __name__ == '__main__':
+#     arr = list(map(int, input().rstrip().split()))
+
+#     miniMaxSum(arr)
+
 from itertools import combinations
 
-# Complete the miniMaxSum function below.
-def miniMaxSum(arr):
-    combos = list(combinations(arr, 4))
+# def find_combos(arr):
+#     combos = list(combinations(arr, 4))
+#     print(combos)
+
+def find_combos(arr):
+    combos = list(combinations(arr, 3))
+    print(combos)
     combo_sums = []
     for combo in combos:
         combo_sums.append(sum(combo))
+    print(combo_sums)
     print(min(combo_sums), max(combo_sums))
 
-if __name__ == '__main__':
-    arr = list(map(int, input().rstrip().split()))
+arr = [1, 2, 3, 4, 5]
 
-    miniMaxSum(arr)
+find_combos(arr)
